c1ffb366a0501afaa7ebe83767b2aa6d8329dc36
Jacob Dees <jacob.dees14@gmail.com>
2020-11-09 22:37:38 -0600

    Purchase Page Modal created and functional

--- src/components/PurchasePage.js
+++ src/components/PurchasePage.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import Modal from 'react-modal';
 
 export default class PurchasePage extends React.Component {
+	state = {
+		modalOpen: false,
+	};
+
+	openModal = () => {
+		this.setState(() => ({ modalOpen: true }));
+	};
+
+	handleClose = () => {
+		this.setState(() => ({ modalOpen: false }));
+	};
+
 	render() {
 		return (
 			<div>
-				<h2>How to buy our balloons!</h2>
+				<Button className="btn-danger" onClick={this.openModal}>
+					Order Here
+				</Button>
+				<Modal
+					isOpen={this.state.modalOpen}
+					onRequestClose={this.handleClose}
+					closeTimeoutMS={200}
+				>
+					<h1>Purchase Information</h1>
+					<form></form>
+				</Modal>
 			</div>
 		);
 	}
