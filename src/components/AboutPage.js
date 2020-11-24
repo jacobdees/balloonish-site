9d6d0fab0f9277a50eb5a90df626c64107dcb76a
Jacob Dees <jacob.dees14@gmail.com>
2020-11-24 17:35:45 -0600

    basic sizing for about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -3,14 +3,18 @@ import React from 'react';
 export default class AboutPage extends React.Component {
 	render() {
 		return (
-			<div className="row my-4">
-				<h2 className="col-md-4 display-4 text-center pb-5">
-					My Story
-				</h2>
-				<p className="col-md-8 text-md-right text-center">
-					Here's where a bio would go. This would also be a good place
-					to put a quick blurb or mission statement about the company.
-				</p>
+			<div className="container">
+				<div className="row my-4 d-flex align-items-center about-page">
+					<h2 className="col-md-4 display-4 text-center pb-5">
+						My Story
+					</h2>
+					<div className="col-md-4"></div>
+					<p className="col-md-4 text-md-right text-center">
+						Here's where a bio would go. This would also be a good
+						place to put a quick blurb or mission statement about
+						the company.
+					</p>
+				</div>
 			</div>
 		);
 	}
