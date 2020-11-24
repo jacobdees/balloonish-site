8260d90fbe69c965cabf6712130053f5306a476c
Jacob Dees <jacob.dees14@gmail.com>
2020-11-24 06:16:34 -0600

    About Page Grid updated

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,8 +4,10 @@ export default class AboutPage extends React.Component {
 	render() {
 		return (
 			<div className="row my-4">
-				<h2 className="col-4 display-4 text-center">My Story</h2>
-				<p className="col-8 text-right">
+				<h2 className="col-sm-4 display-4 text-center pb-5">
+					My Story
+				</h2>
+				<p className="col-sm-8 text-sm-right text-center">
 					Here's where a bio would go. This would also be a good place
 					to put a quick blurb or mission statement about the company.
 				</p>
