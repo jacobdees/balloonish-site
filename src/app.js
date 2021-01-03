ba3be730e9db73a93fee8a83abf7d767c091e9be
Jacob Dees <jacob.dees14@gmail.com>
2021-01-02 18:53:54 -0600

    Setup production build and server

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BalloonishPage from './components/BalloonishPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import BalloonishPage from './components/BalloonishPage';
 
 ReactDOM.render(<BalloonishPage />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,54 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				// This tells webpack to send all .js files to babel-loader so JSX can be rendered
-				loader: 'babel-loader',
-				test: /\.js$/,
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.s?css$/,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /[)]\.(png|svg|jpg|gif|PNG)$/,
-				use: ['file-loader'],
-			},
-		],
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true,
-	},
+module.exports = (env) => {
+	const isProduction = env === 'production';
+	const CSSExtract = new ExtractTextPlugin('styles.css');
+
+	return {
+		entry: './src/app.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js',
+		},
+		module: {
+			rules: [
+				{
+					// This tells webpack to send all .js files to babel-loader so JSX can be rendered
+					loader: 'babel-loader',
+					test: /\.js$/,
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.s?css$/,
+					use: CSSExtract.extract({
+						use: [
+							{
+								loader: 'css-loader',
+								options: {
+									sourceMap: true,
+								},
+							},
+							{
+								loader: 'sass-loader',
+								options: {
+									sourceMap: true,
+								},
+							},
+						],
+					}),
+				},
+				{
+					test: /[)]\.(png|svg|jpg|gif|PNG)$/,
+					use: ['file-loader'],
+				},
+			],
+		},
+		plugins: [CSSExtract],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true,
+		},
+	};
 };
